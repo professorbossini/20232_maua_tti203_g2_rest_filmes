c3e263181fe9c286249e39598b323817aa1c43d8
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-27 12:54:47 -0300

    ap02(django): pagina 37

--- filmes_app/models.py
+++ filmes_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Genero(models.Model):
+  descricao = models.CharField(max_length=100)
+  def __str__(self):
+    return self.descricao
+
 # Create your models here.
 #Object Relational Mapping
 class Filme(models.Model):
