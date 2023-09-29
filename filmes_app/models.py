c858f7975760602bab042c4a64bb2195181e2d46
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-29 09:28:59 -0300

    ap02(django): pagina 35
    - migracao de criacao de genero
    - migracao de insercao de genero padrao

--- filmes_app/models.py
+++ filmes_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 #Object Relational Mapping
+
+class Genero(models.Model):
+  descricao = models.CharField(max_length=100)
+  def __str__(self):
+    return self.descricao
+
 class Filme(models.Model):
   titulo = models.CharField(max_length=100)
   descricao = models.TextField()
